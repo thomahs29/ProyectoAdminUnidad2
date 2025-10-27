6acda774cb97918a4e5fbfa80641a23b768abebf
tho_mahs <tlizana21@alumnos.utalca.cl>
2025-10-27 19:41:02 -0300

    test de conexion de frontend con backend

--- services/backend/src/config/db.js
+++ services/backend/src/config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({ path: "../../../.env" });
 
 const pool = new Pool({
     host: process.env.DB_HOST,
--- services/frontend/vite.config.js
+++ services/frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://backend:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true
       }
     }
